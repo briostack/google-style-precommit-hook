e675b7cfa932dd85602ae736e8dd8627780ab656
Richard Murri <richardmurri@gmail.com>
2020-08-26 16:16:42 -0600

    Update format-code.sh

--- format-code.sh
+++ format-code.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 mkdir -p .cache
 cd .cache
-VERSION=1.4
+VERSION=1.3
 if [ ! -f google-java-format-$VERSION-all-deps.jar ]
 then
     curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-$VERSION/google-java-format-$VERSION-all-deps.jar"
