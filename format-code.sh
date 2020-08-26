a0bb2983c9c5ba2bb241a6a9f495f3abad1b7ef7
Richard Murri <richardmurri@gmail.com>
2020-08-26 16:15:10 -0600

    Update format-code.sh

--- format-code.sh
+++ format-code.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 mkdir -p .cache
 cd .cache
-if [ ! -f google-java-format-1.3-all-deps.jar ]
+VERSION=1.5
+if [ ! -f google-java-format-$VERSION-all-deps.jar ]
 then
-    curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-1.3/google-java-format-1.3-all-deps.jar"
-    chmod 755 google-java-format-1.3-all-deps.jar
+    curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-$VERSION/google-java-format-$VERSION-all-deps.jar"
+    chmod 755 google-java-format-$VERSION-all-deps.jar
 fi
 cd ..
 
-java -jar .cache/google-java-format-1.3-all-deps.jar --replace $@
+java -jar .cache/google-java-format-$VERSION-all-deps.jar --replace $@
